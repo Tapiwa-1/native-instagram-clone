e7f0e822531859aad4dc8d67d8d84b44469215ba
unknown <tmotsi14@gmail.com>
2023-03-09 08:26:40 +0200

    main layout added

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,15 @@
+<script setup>
+import MainLayout from '@/Layouts/MainLayout.vue';
+
+
+</script>
+
+<template>
+    <Head title="Welcome" />
+    
+    <MainLayout>
+        <div class="bg-green-500">Home.vue</div>
+    </MainLayout>
+</template>
+
+
