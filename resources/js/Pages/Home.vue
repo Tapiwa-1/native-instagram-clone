61b7e934877a44880ff1bd9e90652c38f1ded70b
Tapiwa Motsi <tmotsi10@gmail.com>
2023-03-18 17:14:17 +0200

    working on home

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,15 +1,68 @@
 <script setup>
+import { ref, onMounted, toRefs } from 'vue'
+import { Head, Link, router } from '@inertiajs/vue3';
 import MainLayout from '@/Layouts/MainLayout.vue';
 
 
+// import LikesSection from '@/Components/LikesSection.vue'
+// import ShowPostOverlay from '@/Components/ShowPostOverlay.vue'
+import 'vue3-carousel/dist/carousel.css'
+import { Carousel, Slide, Navigation } from 'vue3-carousel'
+import DotsHorizontal from 'vue-material-design-icons/DotsHorizontal.vue';
+
+let wWidth = ref(window.innerWidth)
+let currentSlide = ref(0)
+let currentPost = ref(null)
+let openOverlay = ref(false)
+const props = defineProps({ posts: Object, allUsers: Object })
+const { posts, allUsers } = toRefs(props)
+onMounted(() => {
+    window.addEventListener('resize', () => {
+        wWidth.value = window.innerWidth
+    })
+})
 </script>
 
 <template>
-    <Head title="Welcome" />
-    
+    <Head title="Instagram" />
+
     <MainLayout>
-        <div class="bg-green-500">Home.vue</div>
+        <div class="mx-auto lg:pl-0 md:pl-[80px] pl-0 bg-red-500">
+            <Carousel
+                v-model="currentSlide"
+                class="max-w-[700px] mx-auto"
+                :items-to-show="wWidth >= 768 ? 8 : 6"
+                :items-to-scroll="4"
+                :wrap-around="true"
+                :transition="500"
+                snapAlign="start"
+            >
+
+                <Slide v-for="slide in 10" :key="slide">
+
+                    <Link href="/" class="relative mx-auto text-center mt-4 px-2 cursor-pointer">
+                        <div class="absolute z-[-1] -top-[5px] left-[4px] rounded-full rotate-45 w-[64px] h-[64px] contrast-[1.3]  bg-gradient-to-t from-yellow-300 to-purple-500 via-red-500">
+                            <div class="rounded-full ml-[3px] mt-[3px] w-[58px] h-[58px] bg-white" />
+                        </div>
+                    </Link>
+                </Slide>
+            </Carousel>
+        </div>
     </MainLayout>
+
+
 </template>
+
+<style>
+    .carousel__prev,
+    .carousel__next,
+    .carousel__prev:hover,
+    .carousel__next:hover {
+        color: rgb(49, 49, 49);
+        background-color: rgb(255, 255, 255);
+        border-radius: 100%;
+        margin: 0 20px;
+    }
+</style>
 
 
